89cde491df70b74fc46f76aaffe16505ebfc7f45
willian portela <willianportela1@gmail.com>
2024-03-07 19:01:30 -0300

    fix: uma hora esse cors vai

--- src/app.js
+++ src/app.js
@@ -5,13 +5,20 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "https://lexartfrontend-p0ykseube-will796s-projects.vercel.app",
-      "http://localhost:5173",
-    ],
-  })
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  app.use(cors());
+  next();
+}
+
 );
 app.use(express.json());
 app.use(routers);
